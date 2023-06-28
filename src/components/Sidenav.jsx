133309eb7b539de2cbad11599f4bedae583d5dc7
jonn <joliversalamanca@gmail.com>
2023-06-28 14:58:16 -0400

    modified Sidenav.jsx

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react"
 import { FaAlignJustify } from "react-icons/fa"
 import { HiHome } from "react-icons/hi"
+import {LuNetwork} from "react-icons/lu"
+import {FaBuffer} from "react-icons/fa"
+import {BsFillPersonVcardFill} from "react-icons/bs"
+import {AiOutlineMail} from "react-icons/ai"
 
 export function Sidenav() {
 
@@ -15,20 +19,40 @@ export function Sidenav() {
             <FaAlignJustify onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden" />
 
             {nav ? (
-                <div className="fixed w-full h-sceen bg-white/90 flex flex-col justify-center items-center z-20">
-                    <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-400 m-2 p-4 cursor-pointer-110  ease-in duration-200">
+                <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+                    <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-400 m-2 p-4 cursor-pointer-110  hover:scale-110 ease-in duration-200">
                         <HiHome size={20} />
-                        <span className="pl-4">Home</span>
+                        <span className="pl-4">Inicio</span>
+                    </a>
+                    <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-400 m-2 p-4 cursor-pointer-110  hover:scale-110 ease-in duration-200">
+                        <LuNetwork size={20} />
+                        <span className="pl-4">Trabajo</span>
+                    </a>
+                    <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-400 m-2 p-4 cursor-pointer-110  hover:scale-110 ease-in duration-200">
+                        <FaBuffer size={20} />
+                        <span className="pl-4">Proyecto</span>
+                    </a>
+                    <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-400 m-2 p-4 cursor-pointer-110  hover:scale-110 ease-in duration-200">
+                        <BsFillPersonVcardFill size={20} />
+                        <span className="pl-4">Resumen</span>
+                    </a>
+                    <a href="#main" className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-400 m-2 p-4 cursor-pointer-110  hover:scale-110 ease-in duration-200">
+                        <AiOutlineMail size={20} />
+                        <span className="pl-4">Contacto</span>
                     </a>
 
                 </div>
             ) : (
-                <div>
-
-                </div>
+                " "
             )}
 
-
+              <div className="md:block hidden fixed top[25%] z-10">
+                <div className="flex flex-col">
+                    <a href="#main" className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <HiHome size={20} />
+                    </a>
+                    </div>
+              </div>              
 
 
         </div>
