6afcd8c3f60b9ddc29acfc5e855c32a4298d5bae
yersin77 <pruebacorreoselectronicosjyos@gmail.com>
2023-07-02 07:52:51 -0400

    modified : ProyectoItem.jsx

--- src/components/ProyectoItem.jsx
+++ src/components/ProyectoItem.jsx
@@ -2,7 +2,7 @@ export function ProyectoItem({img, title}){
     return(
          <div className="bg-red-500">
             {/* <div className="bg-green-400 w-ful text-center">holas</div> */}
-            <img src={img} className="w-full h-[300px] object-cover "/>
+            <img src={img} className="w-full h-[350npx] object-cover "/>
          </div>
     )
 }
